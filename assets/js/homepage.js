8bdd4cb238744549b646cec68ce2b3df68562e06
Katharine C Humble <katharinechumble@gmail.com>
2021-07-21 19:41:45 -0400

    added language buttons and associated scripts

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -83,3 +84,34 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 }
+// featured repos function
+//var getFeaturedRepos = function(language) {
+  //  var apiUrl = "https://api.github.com/search/respositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+   // fetch(apiUrl);
+//};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+  };
+
+  // language buttons
+  var buttonClickHandler = function(event) {
+      var language = event.target.getAttribute("data-language");
+      console.log(language);
+      if (language) {
+          getFeaturedRepos(language);
+        //clear old content
+        repoContainerEl.textContent = "";
+      }
+  }
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
